f6ddec92dbd560b0c40ba295b8c54d19b0b81a74
zhongquan <oscarfen@icloud.com>
2019-10-25 18:26:21 -0700

    merging two uthread.c files, and have some changes of MAKEFILE

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -5,14 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
 #include "context.h"
 #include "preempt.h"
 #include "queue.h"
 #include "uthread.h"
 
+
+/* Size of the stack for a thread (in bytes) */
+#define UTHREAD_MAX 32768
 #define UTHREAD_STACK_SIZE 32768
-#define USHRT_MAX 32767
 
 enum state {Running, Ready, Blocked, Zombie};
 static queue_t running;
@@ -22,13 +25,19 @@ static queue_t zombie;
 int initialized =0;
 int num_thread=0;
 
-struct thread{
+struct TCB{
   uthread_t u_tid;
   enum state u_state;
   int retval;
   uthread_ctx_t* u_context;
   void* u_stack;
 };
+
+typedef struct TCB u_thread;
+
+static u_thread* main_thread;
+/* tells create_thread() to create main thread first */
+static int init_tid = 0;
 
 void uthread_yield(void)
 {
@@ -40,17 +49,16 @@ uthread_t uthread_self(void)
 	/* TODO Phase 2 */
 }
 
+/* intialize the main thread */
 void initialize()
 {
+    /* FIXME- need to add the running state for main thread later */
 	initialized=1;
-
 	running = queue_create();
 	ready = queue_create();
 	blocked = queue_create();
 	zombie = queue_create();
-
-	struct thread* main = malloc(sizeof(struct thread));
-
+	u_thread* main = malloc(sizeof(u_thread));
 	main->u_tid = num_thread;//which is 0 right now
 	main->u_state = Running;
 	main->retval=0;
@@ -58,9 +66,7 @@ void initialize()
 	main->u_stack = uthread_ctx_alloc_stack();
 	main->u_context->uc_stack.ss_sp = main->u_stack;
 	main->u_context->uc_stack.ss_size = UTHREAD_STACK_SIZE;
-
 	queue_enqueue(running,main);
-
 	return;
 }
 
@@ -69,21 +75,18 @@ int uthread_create(uthread_func_t func, void *arg)
 	if(!initialized){
 		initialize();
 	}
-
-	if(num_thread++>USHRT_MAX){
+	if(num_thread++>UTHREAD_MAX){
 		fprintf(stderr,"ERROR: TID Overflow!\n");
 		return -1;
 	}
 
-	struct thread* new_thread = malloc(sizeof(struct thread));
-
+	u_thread* new_thread = malloc(sizeof(u_thread));
 	new_thread->u_tid = num_thread;
 	new_thread->u_state = Ready;
 	new_thread->retval = 0;
 	new_thread->u_context = malloc(sizeof(uthread_ctx_t));
 	new_thread->u_stack = uthread_ctx_alloc_stack();
 	int curr_retval = uthread_ctx_init(new_thread->u_context,new_thread->u_stack,func,arg);
-
 	if(curr_retval==-1){
 		return curr_retval;
 	}else{
